58f935d41ad708f27ecacbbcf553b3cdf25b5963
mattyox <109035526+mattyox@users.noreply.github.com>
2023-02-04 19:44:08 +0100

    Uploading missing files

---
+++ Library App/project/authentication/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('view_books')
+
+    if request.method == 'POST':
+
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+
+            user = authenticate(
+                request,
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
+            )
+
+            if user is not None:
+                login(request, user)
+                return redirect('view_books')
+
+            else:
+                context = {'form': form}
+                return render(request, 'authentication/login.html', context)   
+                
+        else:
+            context = {'form': form}
+            return render(request, 'authentication/login.html', context)
+
+    else:    
+        context = {'form': LoginForm()}
+        return render(request, 'authentication/login.html', context)
+
+
+def log_out(request):
+    logout(request)
+    return render(request, 'authentication/logout.html')
